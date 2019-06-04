bd31c5672908f253e0d5c599d2ff1d0cc5df1e6b
AndreiBogorevich <bogorevich@gmail.com>
2019-06-04 13:03:59 +0100

    added withdrawal test

--- Automation/src/test/java/testcases/XYZBank.java
+++ Automation/src/test/java/testcases/XYZBank.java
@@ -1,6 +1,7 @@
 package testcases;
 
 import java.io.IOException;
+import org.openqa.selenium.WebDriver;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -12,37 +13,57 @@ public class XYZBank extends TestBase {
 	private String customer = "Harry Potter";
 	private int amount = 1250;
 
-	@Test
-	public void depositTest() throws IOException{
-		
+	@Test (priority=1)
+	public void depositTest() throws IOException {
+
 		doLogin(customer);
-		
+
 		deposit(amount);
-		
 	}
-	
-	
+
+	@Test (priority=2)
+	public void withdrawalTest() throws IOException {
+
+		doLogin(customer);
+
+		withdraw(amount);
+	}
+
 	public void doLogin(String customerName) throws IOException {
 
 		Assert.assertTrue(setOR("XYZBank", "login"));
+		driver.get(OR.getProperty("url"));
 
 		click("btnCustomerLogin_Xpath");
-		
+
 		Assert.assertTrue(setOR("XYZBank", "customer"));
 		Assert.assertEquals(driver.getCurrentUrl(), OR.getProperty("url"));
 
 		selectByVisibleText("selectYourName_Xpath", customerName);
 		click("btnLogin_Xpath");
 	}
-	
-	public void deposit(int amount){
-		
+
+	public void deposit(int amount) {
+
 		Assert.assertTrue(setOR("XYZBank", "account"));
-		
+
 		click("tabDeposit_Xpath");
 		type("txtAmount_Xpath", Integer.toString(amount));
 		click("btnSubmit_Xpath");
-		
-		Assert.assertEquals(read("labelSuccessfulDeposit_Xpath"), "Deposit Successful");
+
+		Assert.assertEquals(read("labelResult_Xpath").toLowerCase(),
+				"deposit successful");
+	}
+
+	public void withdraw(int amount) {
+
+		Assert.assertTrue(setOR("XYZBank", "account"));
+
+		click("tabWithdrawl_Xpath");
+		type("txtAmount_Xpath", Integer.toString(amount));
+		click("btnSubmit_Xpath");
+
+		Assert.assertEquals(read("labelResult_Xpath").toLowerCase(),
+				"transaction successful");
 	}
 }
