39fc2390b7a3820f13fb2bd0c1eeede57edaf572
AndreiBogorevich <bogorevich@gmail.com>
2019-06-09 18:00:40 +0100

    added Excel utils added CSV utils converted tests to be data-driven

--- Automation/src/test/java/testcases/XYZBank.java
+++ Automation/src/test/java/testcases/XYZBank.java
@@ -1,37 +1,60 @@
 package testcases;
 
 import java.io.IOException;
+import java.util.Hashtable;
 
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import utils.Csv;
 import base.TestBase;
 
 public class XYZBank extends TestBase {
 
-	private String customer = "Harry Potter";
-	private int amount = 1250;
+	private String customer = "Harry Potter"; // to be replaced with data from a
+												// file
+	private static String sDepositTestData = System.getProperty("user.dir")
+			+ "\\src\\test\\resources\\testData\\XYZBank\\depostTest.csv";
 
-	@Test(priority = 1)
-	public void depositTest() throws IOException {
+	@Test(priority = 1, dataProvider = "getDepositData")
+	public void depositTest(Hashtable<String, String> data) throws IOException {
 
 		login(customer);
-		deposit(amount);
+		resetTransactions();
+
+		deposit(data.get("amount"));
+		Assert.assertTrue(setOR("XYZBank", "account"));
+
+		if (data.get("expectedResult").toLowerCase().equals("success")) {
+			Assert.assertEquals(read("lableBalanceValue_Xpath"), // actual
+					data.get("amount")); // expected
+		} else {
+			Assert.assertEquals(read("lableBalanceValue_Xpath"), // actual
+					"0"); // expected
+		}
 	}
 
-	@Test(priority = 2)
-	public void withdrawalTest() throws IOException {
+	// @Test(priority = 2)
+	// public void withdrawalTest() throws IOException {
+	//
+	// login(customer);
+	// withdraw(amount);
+	// }
 
-		login(customer);
-		withdraw(amount);
+	@DataProvider
+	public static Object[][] getDepositData() throws IOException {
+
+		return Csv.getData(sDepositTestData);
 	}
 
 	public void login(String customerName) throws IOException {
 
 		Assert.assertTrue(setOR("XYZBank", "login"));
-		driver.get(OR.getProperty("url"));
+		navigateToUrl();
 
 		click("btnCustomerLogin_Xpath");
+		// it should open another page - which is verified below
 
 		Assert.assertTrue(setOR("XYZBank", "customer"));
 		Assert.assertEquals(driver.getCurrentUrl(), OR.getProperty("url"));
@@ -41,21 +64,23 @@ public class XYZBank extends TestBase {
 		click("btnLogin_Xpath");
 	}
 
-	public void deposit(int amount) {
+	public void deposit(String amount) {
 
 		Assert.assertTrue(setOR("XYZBank", "account"));
+		navigateToUrl();
 
 		click("tabDeposit_Xpath");
-		type("txtAmount_Xpath", Integer.toString(amount));
+		type("txtAmount_Xpath", amount);
 		click("btnSubmit_Xpath");
 
-		Assert.assertEquals(read("labelResult_Xpath").toLowerCase(),
-				"deposit successful");
+		// Assert.assertEquals(read("labelResult_Xpath").toLowerCase(),
+		// "deposit successful");
 	}
 
 	public void withdraw(int amount) {
 
 		Assert.assertTrue(setOR("XYZBank", "account"));
+		navigateToUrl();
 
 		click("tabWithdrawl_Xpath");
 		type("txtAmount_Xpath", Integer.toString(amount));
@@ -63,5 +88,16 @@ public class XYZBank extends TestBase {
 
 		Assert.assertEquals(read("labelResult_Xpath").toLowerCase(),
 				"transaction successful");
+	}
+
+	public void resetTransactions() {
+
+		Assert.assertTrue(setOR("XYZBank", "listTx"));
+		navigateToUrl();
+
+		if (getNumberOfRowsInTable(OR.getProperty("tableTransactions")) > 0) {
+			click("btnReset_Xpath");
+		}
+		driver.navigate().back();
 	}
 }
--- Automation/src/test/java/tmp/tmp.java
+++ Automation/src/test/java/tmp/tmp.java
@@ -1,22 +1,63 @@
 package tmp;
 
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.Properties;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.Map;
 
-import org.openqa.selenium.WebDriver;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-import base.Browsers;
-import base.TestBase;
+public class tmp {
 
-public class tmp extends TestBase{
+	private static String sFile = System.getProperty("user.dir")
+			+ "\\src\\test\\resources\\testData\\sample.csv";
 
-//	@Test
-//	public void temporaryTest() throws IOException, InterruptedException {
-//
-//		Thread.sleep (5000L);
-//
-//	}
+	@Test(dataProvider = "getData")
+	public void testData(Hashtable<String, String> data) {
 
+		System.out.println(data.get("name") + "---" + data.get("age") + "---"
+				+ data.get("gender"));
+
+	}
+
+	// Hashtable
+
+	@DataProvider
+	public static Object[][] getData() throws IOException {
+
+		try (Reader reader = Files.newBufferedReader(Paths.get(sFile));
+
+		CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT);
+
+		) {
+			Map<String, String> map = new HashMap();
+
+			List<CSVRecord> csvRecordsList = csvParser.getRecords();
+
+			int rows = csvRecordsList.size();
+			int cols = csvRecordsList.get(0).size();
+			CSVRecord csvHeader = csvRecordsList.get(0);
+
+			Object[][] data = new Object[rows - 1][1];
+			Hashtable<String, String> table = null;
+
+			for (int i = 1; i < rows; i++) {
+				table = new Hashtable<String, String>();
+				CSVRecord csvRecord = csvRecordsList.get(i);
+				for (int y = 0; y < cols; y++) {
+					table.put(csvHeader.get(y), csvRecord.get(y));
+					data[i - 1][0] = table;
+				}
+			}
+			return data;
+		}
+	}
 }
